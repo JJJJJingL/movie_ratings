2845f8f49b4ffe5f48eaf5ad892fe09509b2bce8
siyu <ls1289@georgetown.edu>
2019-04-07 19:12:40 -0400

    update

--- dialogue_extraction.py
+++ dialogue_extraction.py
@@ -1,25 +1,38 @@
 import os
 import argparse
+import re
 
-def main(dialogues_foler):
+def get_filenames(folder_name):
     fis = []
     n = 0
-    print(dialogues_foler)
-    rootdir = os.walk(dialogues_foler)
+    rootdir = os.walk(folder_name)
     for root, subdir, files in rootdir:
         for name in files:
             if name.endswith('.txt'):
                 n += 1
                 fis.append(os.path.join(root, name))
+    return fis
 
-    print(fis)
-    print(len(fis))
-    # for dir in os.path.listdir(main_dir):
-    #     print(dir)
-        # for file in os.path.listdir(dir):
-        #     with open(os.path.join(dir, file), "r") as fd:
-        #         file_content.append(fd.read)
+def main(dialogues_folder):
 
+    the_ending = r'(.*)(the)\b'
+    files_name = get_filenames(dialogues_folder)
+    #print(files_name)
+    dialogue_text = []
+    dialogue_name = []
+    for dir in files_name:
+        with open(dir, "r") as file:
+            text = file.read()
+            name = os.path.splitext(os.path.basename(dir))[0]
+            name = name.split("_")[0]
+            match_the = re.search(the_ending, name)
+            if match_the is not None:
+                name = re.sub(the_ending, r'\2\1', name)
+            dialogue_name.append(name)
+            dialogue_text.append(text)
+    print(len(dialogue_text))
+    #print(file_content[0])
+    print(dialogue_name[0])
 
 if __name__ == "__main__":
 
