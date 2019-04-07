d4a2c60eb240ed502a2f9d629b0f0eb3b09dd54b
IvyWang13 <47639849+IvyWang13@users.noreply.github.com>
2019-04-07 18:28:46 -0400

    Add files via upload

--- movie_data.py
+++ movie_data.py
@@ -2,26 +2,48 @@
 from collections import defaultdict
 id_dict = defaultdict(list)
 id_to_rating = "/Users/ivywang/PycharmProjects/movie_final/title.ratings.tsv"
-id_to_attr = '/Users/ivywang/PycharmProjects/movie_final/title.akas.tsv'
-with open(id_to_rating, 'r') as rating:
-    next(rating)
-    for line in rating:
-        fields = line.strip().split('\t')
-        id_dict[fields[0]].append(fields[1])
-        id_dict[fields[0]].append(fields[2])
-# print(id_dict)
+id_to_attr = '/Users/ivywang/PycharmProjects/movie_ratings/data.tsv'
 
+# append those that: type is either short or movie, append title, append start/end year if there is any; append year
+# that is more than 1950; append genre, lowered. Title is also lowered and stripped of spaces
 with open(id_to_attr, 'r') as attr:
     next(attr)
     for line in attr:
         fields = line.strip().split('\t')
-        if fields[3] == 'US':
-            if fields[0] in id_dict.keys(): # check if the rating exists
-                title = fields[2].lower()
-                title = "".join(title.split())
-                id_dict[fields[0]].append(title) # append title
+        if fields[1] == 'movie' or fields[1] == 'short':
+            title = fields[2].lower()
+            title = "".join(title.split())
+            id_dict[fields[0]].append(title) # append title
+            if len(fields[5]) > 3 and fields[5] is not None:
+                year = int(fields[5])
+            elif len(fields[6]) > 3 and fields[6] is not None:
+                year = int(fields[6])
+            if year > 1950:
+                id_dict[fields[0]].append(year)
+            if fields[8] is not None:
+                id_dict[fields[0]].append(fields[8].lower()) # genre
 
-print(id_dict)
+# append raitng and numVotes
+with open(id_to_rating, 'r') as rating:
+    next(rating)
+    for line in rating:
+        r_fields = line.strip().split('\t')
+        if r_fields[0] in id_dict.keys():
+            id_dict[r_fields[0]].append(r_fields[1]) # rating
+            id_dict[r_fields[0]].append(r_fields[2]) # number of votes
+
+
+# remove from the ones that do not have ratings
+for i, j in list(id_dict.items()):
+    if len(j) < 4:
+        del id_dict[i]
+for i, j in id_dict.items():
+    print(i)
+    print(j)
+
+print(len(id_dict.keys())) # returns 341306 films
+
+
 
 movie_name_list = '15minutes'
 dialog_filename = '15minutes_dialog.txt'
